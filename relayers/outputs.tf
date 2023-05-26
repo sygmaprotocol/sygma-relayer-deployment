9d74108cd606c07c8afed7c5f7a500bf4ab495a0
Idris Isah <58168886+eedygreen@users.noreply.github.com>
2023-05-26 19:48:08 +0100

    Use Dynamic Variable for output file (#27)
    * permissions: getSecretValue
    
    * fixed: dynamic variables
    
    * set default variable to 1
    
    * ignore dns_address file

--- relayers/outputs.tf
+++ relayers/outputs.tf
@@ -1,11 +1,22 @@
-output "lb_dns_0" {
-  value = aws_lb.main[0].dns_name
+resource "local_file" "dns" {
+  count    = var.relayers
+  content  = aws_lb.main[count.index].dns_name
+  filename = "${path.module}/dns/dns.${count.index}"
 }
 
-output "lb_dns_1" {
-  value = aws_lb.main[1].dns_name
-}
-
-output "lb_dns_2" {
-  value = aws_lb.main[2].dns_name
+resource "null_resource" "dns" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    working_dir = "${path.module}/dns/"
+    command = <<EOF
+      rm dns_address
+      for i in dns.*; do (cat $i; echo '') >> dns_address; done
+      rm dns.*
+    EOF
+  }
+  depends_on = [
+    local_file.dns
+  ]
 }
