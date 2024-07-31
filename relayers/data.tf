6f7da365256a3ec94f807fc1840a13397f4c5b6d
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2024-07-31 11:26:54 +0100

    tls/ssl policy

--- relayers/data.tf
+++ relayers/data.tf
@@ -29,6 +29,6 @@ data "aws_subnets" "ec2_public_subnets" {
 }
 
 data "aws_acm_certificate" "chainsafe_io" {
-  domain   = "*.chainsafe.io"
+  domain   = "*.chainsafe.io"  // use your own domain here
   statuses = ["ISSUED"]
 }
